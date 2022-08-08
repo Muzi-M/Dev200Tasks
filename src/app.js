77bb5896c504eb57bbfa0d7f3b3f7fd4eca09924
Muziwethu Mlotshwa <diff.mlot@gmail.com>
2022-08-08 19:13:31 +0200

    added more functionality

--- src/app.js
+++ src/app.js
@@ -1,46 +1,63 @@
 const todayDateAndTime = "2016-05-18T11:27:00";
 const todayDate = todayDateAndTime.slice(0, 10);
-const currentTime = todayDateAndTime.slice(11, 16);
 
-console.log(currentTime);
+const currentTime = () => {
+  slicedTime = todayDateAndTime.slice(11, 16);
+  if (slicedTime[3] == 0 && slicedTime[4] == 0) {
+    return parseInt(slicedTime.slice(0, 2));
+  } else {
+    return parseInt(slicedTime.slice(0, 2)) + 1;
+  }
+};
 
-const checkSlotAvailability = (time, jobLength, availability) => {
+const checkSlotAvailability = (time, jobLength, date, availability) => {
+  if (date === todayDate) {
+    return presentDay(time, jobLength, availability);
+  }
+
+  return checkAvailabilityStatus(time, jobLength, availability);
+};
+
+const checkAvailabilityStatus = (time, jobLength, availability) => {
   travelBeforeBuffer = time - 1;
   travelAfterBuffer = time + jobLength + 1;
   if (availability.includes(time)) {
-    checkAvailable(travelBeforeBuffer, travelAfterBuffer, availability);
-    checkUnavailable(time, travelBeforeBuffer, travelAfterBuffer, availability);
+    if (time === 17 || time === 9) {
+      if (time === 17) {
+        if (jobLength === 1 && availability.includes(travelBeforeBuffer)) {
+          return "AVAILABLE";
+        } else {
+          return "UNAVAILABLE";
+        }
+      } else if (time === 9 && availability.includes(travelAfterBuffer)) {
+        return "AVAILABLE";
+      }
+    } else if (
+      availability.includes(travelBeforeBuffer) &&
+      availability.includes(travelAfterBuffer)
+    ) {
+      return "AVAILABLE";
+    } else if (
+      !availability.includes(travelBeforeBuffer) ||
+      !availability.includes(travelAfterBuffer)
+    ) {
+      return "UNAVAILABLE";
+    }
   } else {
     return "FULL";
   }
 };
 
-const checkAvailable = (buffer1, buffer2, availability) => {
-  const timeBuffers = [buffer1, buffer2];
-  if (buffer1 === 8 && buffer2 !== 17) {
-    timeBuffers.shift();
-  } else if (buffer2 === 18 && buffer1 !== 8) {
-    timeBuffers.pop();
-  }
-  const timeSlotAvailable = timeBuffers.every((value) => {
-    return availability.includes(value);
-  });
-  if (timeSlotAvailable) {
-    return "AVAILABLE";
-  }
-};
-
-const checkUnavailable = (time, buffer1, buffer2, availability) => {
-  if (
-    (availability.includes(time) && !availability.includes(buffer1)) ||
-    !availability.includes(buffer2)
-  ) {
-    return "UNAVAILABLE";
-  }
-};
-
-console.log(checkSlotAvailability(15, 1, [9, 10, 14, 15, 16, 17]));
-
 const presentDay = (time, jobLength, availability) => {
-  for (let i = 0; i < availability.length; i++) {}
+  travelBeforeBuffer = time - 2;
+  travelAfterBuffer = time + jobLength + 1;
+  if (time <= currentTime()) {
+    return "UNAVAILABLE";
+  } else {
+    return checkAvailabilityStatus(time, jobLength, availability);
+  }
 };
+
+console.log(
+  checkSlotAvailability(1, 1, "2016-05-18", [9, 10, 11, 12, 13, 14, 17])
+);
